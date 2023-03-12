5e15024922931a77c9a2f329413793813f4ff797
aralnu <arturo.alnu@gmail.com>
2023-03-11 22:00:53 -0600

    changed grid to flexbox and added terms and conditions

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//selectors
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#sobre-nosotros");
+
+//Smooth scrolling navbar
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//smooth scrolling footer
+
+document
+  .querySelector(".footer__links")
+  .addEventListener("click", function (e) {
+    if (e.target.classList.contains("footer__terms")) {
+      return true;
+    } // this removes the prevent default line for the "terms and conditions" link in the footer nav, cause preventdefault breaks the popup opening
+    e.preventDefault();
+    //Matching strategy
+    if (e.target.classList.contains("footer__link")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+//Smooth scrolling button
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
